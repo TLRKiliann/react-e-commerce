77867cb78fa425ec2ae433b83d0f10068a6ae9a2
TLRKiliann <philogenie@protonmail.com>
2022-07-04 19:32:35 +0200

    config setup and styled changed

--- shoppping/src/components/StoreItem.tsx
+++ shoppping/src/components/StoreItem.tsx
@@ -23,7 +23,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 <Card.Title
                     className="d-flex justify-content-between 
                         align-items-baseline mb-4">
-                    <span className="fs-2">{name}</span>
+                    <span className="fs-4">{name}</span>
                     <span 
                         className="ms-2 text-muted"
                     >
