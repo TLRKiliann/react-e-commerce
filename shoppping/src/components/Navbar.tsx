c7d27558fc49805c9f619ec03e7f40006afcd50b
TLRKiliann <philogenie@protonmail.com>
2022-07-05 11:59:17 +0200

    multiples changes

--- shoppping/src/components/Navbar.tsx
+++ shoppping/src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 //import '../utilities/shopping-cart-svgrepo-com.svg'
-
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar() {
+  const {openCart, cartQuantity} = useShoppingCart()
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
       <Container>
@@ -18,7 +19,9 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
-          <Button 
+        {cartQuantity > 0 && (
+          <Button
+            onClick={openCart} 
             style={{ width: "3rem", height: "3rem", position: "relative" }}
             variant="outline-primary"
             className="rounded-circle"
@@ -51,9 +54,10 @@ export function Navbar() {
                 transform: "translate(25%, 25%)",
               }}
             >
-             3
+             {cartQuantity}
             </div>
           </Button>
+          )}
       </Container>
     </NavbarBs>
   )
